cbc44302426fd08edddd64aebff328ae6a9636ab
David McLennan <dmclennan92@gmail.com>
2018-06-17 22:21:25 +0100

    Added SelectorOnTop ListView control and Android renderer
    Ripple and hold colour now appear on top of image frames in listview.
    Effect may not be clear enough on darker images, could try to modify colours.

---
+++ src/SimpleMeal/SimpleMeal.Android/Renderers/SelectorOnTopListViewRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SimpleMeal.Controls;
+using SimpleMeal.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SelectorOnTopListView), typeof(SelectorOnTopListViewRenderer))]
+namespace SimpleMeal.Droid.Renderers
+{
+    public class SelectorOnTopListViewRenderer : ListViewRenderer
+    {
+        public SelectorOnTopListViewRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            // Works, but isn't clear enough on some images
+            // Could customise the tap/hold effect colour
+            Control.SetDrawSelectorOnTop(true);
+        }
+    }
+}
---
+++ src/SimpleMeal/SimpleMeal/Controls/SelectorOnTopListView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SimpleMeal.Controls
+{
+    public class SelectorOnTopListView : ListView
+    {
+        public SelectorOnTopListView(ListViewCachingStrategy strategy) : base(strategy) { }
+    }
+}
