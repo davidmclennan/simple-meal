901fdea7aa975722786b25e3430e1689b623990a
David McLennan <dmclennan92@gmail.com>
2018-06-18 21:59:46 +0100

    Created native padding effect
    Currently using explicit values

---
+++ src/SimpleMeal/SimpleMeal.Android/Effects/NativePaddingEffect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SimpleMeal.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("SimpleMeal")]
+[assembly: ExportEffect(typeof(NativePaddingEffect), "NativePaddingEffect")]
+namespace SimpleMeal.Droid.Effects
+{
+    public class NativePaddingEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (Control != null)
+                Control.SetPadding(50, 50, 50, 50);
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ src/SimpleMeal/SimpleMeal/Effects/NativePaddingEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SimpleMeal.Effects
+{
+    public class NativePaddingEffect : RoutingEffect
+    {
+        public NativePaddingEffect() : base("SimpleMeal.NativePaddingEffect") { }
+    }
+}
