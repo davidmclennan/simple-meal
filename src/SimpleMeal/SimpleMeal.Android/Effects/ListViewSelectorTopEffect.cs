0d5381d442d5ac2d778a39336efd913fe5f7a8db
David McLennan <dmclennan92@gmail.com>
2018-06-20 20:25:15 +0100

    Changed ListView selector on top from renderer to effect

---
+++ src/SimpleMeal/SimpleMeal.Android/Effects/ListViewSelectorTopEffect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SimpleMeal.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(ListViewSelectorTopEffect), "ListViewSelectorTopEffect")]
+namespace SimpleMeal.Droid.Effects
+{
+    public class ListViewSelectorTopEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var listView = Control as AbsListView;
+
+                if (listView != null)
+                    listView.SetDrawSelectorOnTop(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ src/SimpleMeal/SimpleMeal/Effects/ListViewSelectorTopEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SimpleMeal.Effects
+{
+    public class ListViewSelectorTopEffect : RoutingEffect
+    {
+        public ListViewSelectorTopEffect() : base("SimpleMeal.ListViewSelectorTopEffect") { }
+    }
+}
