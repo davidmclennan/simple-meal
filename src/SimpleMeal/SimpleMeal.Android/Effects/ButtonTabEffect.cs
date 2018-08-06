e4b7b97bfaf034fb0b95320eb1daee88072e25a3
David McLennan <dmclennan92@gmail.com>
2018-08-06 19:52:04 +0100

    Replaced button tab boxview underline with effect and android drawable

---
+++ src/SimpleMeal/SimpleMeal.Android/Effects/ButtonTabEffect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.Content;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using SimpleMeal.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(ButtonTabEffect), "ButtonTabEffect")]
+namespace SimpleMeal.Droid.Effects
+{
+    public class ButtonTabEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var button = Control as AppCompatButton;
+
+                if (button != null)
+                    button.Background = ContextCompat.GetDrawable(button.Context, Resource.Drawable.btn_tab_selector);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
---
+++ src/SimpleMeal/SimpleMeal/Effects/ButtonTabEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SimpleMeal.Effects
+{
+    public class ButtonTabEffect : RoutingEffect
+    {
+        public ButtonTabEffect() : base("SimpleMeal.ButtonTabEffect") { }
+    }
+}
